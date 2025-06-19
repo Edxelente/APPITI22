ce4f54b6f22f668aa24298cbe90e635cd91abd71
Edxel Villarreal <edxelyahir.vl@gmail.com>
2025-06-19 11:43:01 -0500

    Corregido

--- src/app/pagina/checkbox/checkbox.page.ts
+++ src/app/pagina/checkbox/checkbox.page.ts
@@ -2,14 +2,21 @@ import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { IonicModule } from '@ionic/angular';
+import { ComponentesModule } from 'src/app/componentes/componentes.module';
 
+interface Alumno{
+  name: string;
+  beca: string;
+}
 
 @Component({
   selector: 'app-checkbox',
   templateUrl: './checkbox.page.html',
   styleUrls: ['./checkbox.page.scss'],
-  imports: [IonicModule, CommonModule, RouterModule],
+  imports: [IonicModule, CommonModule, RouterModule, ComponentesModule],
 })
+
+
 export class CheckboxPage implements OnInit {
 
   constructor() { }
@@ -17,4 +24,14 @@ export class CheckboxPage implements OnInit {
   ngOnInit() {
   }
 
+infoNombres: Alumno[] = [
+  {
+  name: 'Edxel Villarreal',
+  beca: 'false'
+},
+  {
+  name: 'Yahir Lopez',
+  beca: 'false'
+}
+]
 }
